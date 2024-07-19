764fc78019c5533317dbaa24952cb6180a0c2864
doraotari <doraotari@gmail.com>
2024-07-19 04:53:54 +0000

    19 juli

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/format_rupiah.dart';
 import 'package:myapp/produk.dart';
+import 'package:myapp/provider.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({super.key, required this.data});
@@ -13,28 +15,111 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 40),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 200,
-              child: GridTile(
-                header: Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
-                    onPressed: () => _kembali(context),
-                    icon: const Icon(Icons.arrow_back_ios),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 200,
+                child: GridTile(
+                  header: Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      onPressed: () => _kembali(context),
+                      icon: const Icon(Icons.arrow_back_ios),
+                    ),
+                  ),
+                  child: Image.network(
+                    data.image,
                   ),
                 ),
-                child: Image.network(
-                  data.image,
+              ),
+              Text(data.category.toUpperCase()),
+              Text(
+                data.title,
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                formatRupiah(data.price),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(data.rate.toString()),
+                      const Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      )
+                    ],
+                  ),
+                  Text('${data.count} Terjual'),
+                ],
+              ),
+              Card(
+                color: Colors.amber.shade100,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Text(data.description),
                 ),
               ),
-            ),
-          ],
+              const Text(
+                'Produk Serupa',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              BagianProdukSerupa(
+                category: data.category,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+// tampilan produk serupa
+class BagianProdukSerupa extends StatelessWidget {
+  const BagianProdukSerupa({super.key, required this.category});
+  final String category;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: FutureBuilder(
+          future: ambilProdukKategori(category),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: LinearProgressIndicator());
+            }
+            List<Produk> listProduk = snapshot.data!;
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: listProduk.length,
+              itemBuilder: (context, index) {
+                Produk produk = listProduk[index];
+                return Container(
+                  width: 130,
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Expanded(child: Image.network(produk.image)),
+                        Text(
+                          produk.title,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(formatRupiah(produk.price)),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 }
--- lib/provider.dart
+++ lib/provider.dart
@@ -7,7 +7,25 @@ Future<List<Produk>> ambilProduk() async {
   final respon = await http.get(Uri.parse('https://fakestoreapi.com/products'));
   if (respon.statusCode == 200) {
     final List data = jsonDecode(respon.body);
-    return data.map((json) => Produk.fromJson(json),).toList();
+    return data
+        .map(
+          (json) => Produk.fromJson(json),
+        )
+        .toList();
   }
   throw Exception('Gagal ambil data dari internet');
+}
+
+Future<List<Produk>> ambilProdukKategori(category) async {
+  final respon = await http
+      .get(Uri.parse('https://fakestoreapi.com/products/category/$category'));
+  if (respon.statusCode == 200) {
+    final List<dynamic> data = jsonDecode(respon.body);
+    return data
+        .map(
+          (json) => Produk.fromJson(json),
+        )
+        .toList();
+  }
+  throw Exception('Gagal ambil kategori dari internet');
 }
